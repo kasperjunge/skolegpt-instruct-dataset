82a8e86f89606115cb29a8a8fd7f246f69982481
Kasper Junge <kasperjunge@MBPtilhdeKasper.lan>
2024-01-26 07:44:43 +0100

    .

--- skolegpt_instruct_dataset/translate.py
+++ skolegpt_instruct_dataset/translate.py
@@ -1,14 +1,25 @@
 import os
+from datetime import datetime
 
+import polars as pl
 import requests
 from dotenv import load_dotenv
+from tqdm import tqdm
+
+from .config import config
 
 load_dotenv()
 
 auth_key = os.environ["DEEPL_API_KEY"]
 
 
-def translate_with_deepl(text, target_lang):
+def translate_dataset(df: pl.DataFrame, save_freq: int) -> pl.DataFrame:
+    df = translate_system_prompts(df)
+    df = translate_examples(df, save_freq=save_freq)
+    return df
+
+
+def translate_with_deepl(text: str, target_lang: str):
     """
     Translates text using the DeepL API.
 
@@ -21,7 +32,7 @@ def translate_with_deepl(text, target_lang):
         str: The translated text or an error message.
     """
     # URL for the DeepL API
-    url = "https://api-free.deepl.com/v2/translate"
+    url = "https://api.deepl.com/v2/translate"
 
     # Define the request headers
     headers = {
@@ -47,3 +58,56 @@ def translate_with_deepl(text, target_lang):
     else:
         # Return an error message if the request was not successful
         return f"Error: {response.status_code} - {response.text}"
+
+
+def translate_system_prompts(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Translates unique system prompts in a DataFrame to Danish using DeepL.
+
+    This function iterates over unique system prompts found in the specified column of the DataFrame.
+    Each prompt is translated to Danish, and these translations are then used to update the original DataFrame.
+
+    Args:
+    - dataframe (pl.DataFrame): A DataFrame containing a column 'system_prompt' with prompts to be translated.
+
+    Returns:
+    - pl.DataFrame: A DataFrame with the 'system_prompt' column updated with Danish translations.
+    """
+
+    # Extract unique system prompts from the DataFrame
+    unique_system_prompts = df["system_prompt"].unique().to_list()
+
+    # Dictionary to hold translations
+    translated_system_prompts = {}
+
+    # Translate each unique system prompt to Danish
+    for system_prompt in unique_system_prompts:
+        translated_system_prompts.update(
+            {system_prompt: translate_with_deepl(system_prompt, "DA")}
+        )
+
+    # Update the DataFrame with translated prompts
+    df = df.with_columns(pl.col("system_prompt").replace(translated_system_prompts))
+
+    return df
+
+
+def translate_examples(df: pl.DataFrame, save_freq: int) -> pl.DataFrame:
+    date = datetime.now().isoformat()[:19]
+
+    n_translations = 0
+    translated_rows = []
+    try:
+        for r in tqdm(df.to_dicts(), total=len(df)):
+            r["question"] = translate_with_deepl(r["question"], "DA")
+            r["response"] = translate_with_deepl(r["response"], "DA")
+            translated_rows.append(r)
+            n_translations += 1
+            if (n_translations % save_freq) == 0:
+                pl.DataFrame(translated_rows).write_parquet(
+                    config.data_dir / f"save_{date}.parquet"
+                )
+    except:
+        breakpoint()
+
+    return pl.DataFrame(translated_rows)
